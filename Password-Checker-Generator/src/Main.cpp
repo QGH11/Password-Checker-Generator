147271078e508791a74a8cfcdc47fec56711ff01
QGH <Guanhua.Qiao@outlook.com>
2021-07-10 16:22:23 -0700

    Finished

--- Password-Checker-Generator/src/Main.cpp
+++ Password-Checker-Generator/src/Main.cpp
@@ -14,62 +14,164 @@ using namespace std;
 
 int askForOptions();
 bool ifContinue();
-string askForInput();
+string passwordGenerator();
+string checkPasswordStrength();
+bool checkUppercase(string password);
+bool checkSymbol(string password);
+
 
 // ask user for options
 int askForOptions() {
 	int decision;
-	cout << "To check password strength, type \"1\".\nTo generate strong password type \"2\".\n";
+	cout << "To generate strong password type \"1\".\nTo check password strength type \"2\".\n";
 	// if user input is not right, warning and ask again
 	cin >> decision;
 	cout << "\n";
+
+	while (cin.fail())
+	{
+		// user didn't input a number
+		cin.clear(); // reset failbit
+		cin.ignore(numeric_limits<streamsize>::max(), '\n'); //skip bad input
+		// next, request user reinput
+		cout << "Please input an integer!\n";
+		cout << "To generate strong password type \"1\".\nTo check password strength type \"2\".\n";
+		// if user input is not right, warning and ask again
+		cin >> decision;
+	}
 
 	return decision;
 }
 
 // ask user if continue 
 bool ifContinue() {
-	string decision; 
-	cout << "To continue type \"yes\".\nTo stop type \"no\"\n";
-	cin >> decision;
-	cout << "\n";
-	
-	//convert string into uppercase
-	for (int i = 0; i < decision.length(); i++) {
-		decision[i] = toupper(decision[i]);
-	}
-	cout << decision;
+	string decision;
 
-	if (decision.compare("YES")) {
-		return true;
+		cout << "\nTo continue type \"yes\".\nTo stop type \"no\"\n";
+		cin >> decision;
+		cout << "\n";
+
+		// convert string into uppercase
+		for (int i = 0; i < decision.length(); i++) {
+			decision[i] = toupper(decision[i]);
+		}
+		
+		// check for valid input
+		while (!(decision.compare("YES") == 0 || decision.compare("NO") == 0)) {
+			cout << "Invalid input!\n";
+
+			cout << "\nTo continue type \"yes\".\nTo stop type \"no\"\n";
+			cin >> decision;
+			cout << "\n";
+
+			// convert string into uppercase
+			for (int i = 0; i < decision.length(); i++) {
+				decision[i] = toupper(decision[i]);
+			}
+		} 
+
+		if (decision.compare("YES")) {
+			return true;
+		}
+		else if (decision.compare("NO")) {
+			return false;
+		}
+	
+}
+
+// generate strong passwords
+string passwordGenerator() {
+	string password;
+	char alphnum[] = "0123456789" "!@#$%^&*" "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz";
+	int alphnumLength = sizeof(alphnum);
+
+	int passwordLength;
+	do {
+		cout << "Length of your strong password (at least 8 letters): ";
+		cin >> passwordLength;
+
+		// make sure valid input
+		while (cin.fail())
+		{
+			// user didn't input a number
+			cin.clear(); // reset failbit
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //skip bad input
+			// next, request user reinput
+			cout << "Please input an integer!\n";
+			cout << "Length of your strong password (at least 8 letters): ";
+			cin >> passwordLength;
+		}
+
+		if (passwordLength < 8 || passwordLength > 100) {
+			cout << "Invalid password length! (8 < Length < 100) \n";
+		}
+	} while (passwordLength < 8 || passwordLength > 100);
+
+
+	for (int i = 0; i < passwordLength; i++) {
+		password = password + alphnum[rand() % alphnumLength];
+	}
+	
+	return password;
+}
+
+// check user password strength
+string checkPasswordStrength() {
+
+	string userPassword;
+
+	cout << "Your password: ";
+	cin >> userPassword;
+
+	if (userPassword.length() < 8) {
+		return "Weak, not long enought; minimun 8 letters!";
+	}
+	// no letter or uppercase letter
+	else if (!checkUppercase(userPassword)) { 
+		return "Weak, no uppercase letters or any letters!";
+	} 
+	else  if (!checkSymbol(userPassword)) {
+		return "Weak, no sepcial symbols/characters!";
 	}
 	else {
-		return false;
+		return "Strong password!";
 	}
 }
 
-// ask user for inputs
-string askForInput() {
-
-	string input;
-
-
+// check if there is at least one uppercase or any letter
+bool checkUppercase(string password) {
+	for (int i = 0; i < password.length(); i++) {
+		if (isupper(password[i])) {
+			return true;
+		}
+	}
 	
-	return input;
+	return false;
+}
+
+// check if there is at least one symbol
+bool checkSymbol(string password) {
+	for (int i = 0; i < password.length(); i++) {
+		if (!isalpha(password[i]) && !isdigit(password[i])) {
+			return true;
+		}
+	}
+	return false;
 }
 
 // main functions
 int main() {
 	
-	bool check = ifContinue();
-	cout << check;
-	while (ifContinue()) {
+	 do {
 		if (askForOptions() == 1) {
-			cout <<"you choose 1";
+			srand(time(NULL));
+			cout << passwordGenerator() << "\n";
 		}
 		else {
-			cout << "you choose 2";
+			do {
+				cout << checkPasswordStrength() << "\n";
+			} while (!ifContinue());
 		}
-	}
+	 } while (!ifContinue());
 
 }
